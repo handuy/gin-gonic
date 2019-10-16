2f9584c3c5d89bc430291c7964d1c0f3311e408b
DuyTechmaster <duy@techmaster.vn>
2019-10-16 09:23:21 +0700

    hello world

---
+++ hello-world/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello go-gin")
+	})
+	router.Run(":8085")
+}

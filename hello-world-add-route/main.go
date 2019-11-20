6014f61abe48274c656537fbdb74e1b9e03eeb70
DuyTechmaster <duy@techmaster.vn>
2019-11-20 18:01:02 +0700

    cập nhật ví dụ hello-world và hello-world-add-route

--- hello-world-add-route/main.go
+++ hello-world-add-route/main.go
@@ -1,25 +1,69 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
+type Issue struct {
+	ID string
+	Title string
+}
+
 func main() {
 	router := gin.Default()
+
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello World")
 	})
-	router.GET("/about", func(c *gin.Context) {
-		c.String(200, "This is the about page")
-	})
-	router.GET("/khoa-hoc/:id/:name", func(c *gin.Context) {
-		courseId := c.Param("id")
-		courseName := c.Param("name")
-		courseInfo := fmt.Sprintf("Thông tin khoá học: id khoá học là %s và tên khoá học là %s", courseId, courseName)
 
-		c.String(200, courseInfo)
+	router.GET("/issues", func(c *gin.Context) {
+		var result = []Issue{
+			{
+				ID: "1",
+				Title: "Issue số 1",
+			},
+			{
+				ID: "2",
+				Title: "Issue số 2",
+			},
+		}
+
+		c.JSON(200, result)
 	})
-	router.Run(":8080")
+
+	router.GET("/issues/:id", func(c *gin.Context) {
+		// Giả sử đây là danh sách các issue lưu trong database
+		var listIssues = []Issue{
+			{
+				ID: "1",
+				Title: "Issue số 1",
+			},
+			{
+				ID: "2",
+				Title: "Issue số 2",
+			},
+		}
+
+		// result là kết quả trả về client
+		var result = Issue{
+			ID: "",
+			Title: "",
+		}
+
+		// Lấy ID của issue mà người dùng muốn xem
+		var issueId = c.Param("id")
+
+		// Giả lập việc kiểm tra trong database xem có issue đó ko
+		// Sau này khi kết nối đến database thì sẽ dùng lệnh SELECT để kiểm tra
+		for _, v := range listIssues{
+			if v.ID == issueId {
+				result = v
+				break
+			}
+		}
+
+		c.JSON(200, result)
+	})
+
+	router.Run(":8081")
 }
--- hello-world/main.go
+++ hello-world/main.go
@@ -2,10 +2,30 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Issue struct {
+	ID string
+	Title string
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello World")
 	})
-	router.Run(":8080")
+
+	router.GET("/issues", func(c *gin.Context) {
+		var result = []Issue{
+			{
+				ID: "1",
+				Title: "Issue số 1",
+			},
+			{
+				ID: "2",
+				Title: "Issue số 2",
+			},
+		}
+
+		c.JSON(200, result)
+	})
+	router.Run(":8081")
 }
